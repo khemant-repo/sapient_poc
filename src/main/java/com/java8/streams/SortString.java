4dbcfc530e4ee6f9654e6ecb48a7b838e2d736a3
Hemant Kumar <khemant.hubworks@gmail.com>
2025-08-01 13:28:29 +0530

    Added DSA problems on array and string

--- src/main/java/com/java8/streams/SortString.java
+++ src/main/java/com/java8/streams/SortString.java
@@ -8,11 +8,11 @@ import java.util.stream.Collectors;
 public class SortString {
     public static void main(String[] args) {
         String str = "hhhtttbbbaaa";
-        String result = str.chars()
-                .mapToObj(c-> (char) c)
-                .sorted()
-                .map(String::valueOf)
-                .collect(Collectors.joining());
+        String result = str.chars()// convert string to chars : return IntStream
+                .mapToObj(c-> (char) c) // convert int to char(eg: 104 = 'h')
+                .sorted()// sort characters
+                .map(String::valueOf) // convert characters to string
+                .collect(Collectors.joining()); //// join all characters to string
 
         System.out.println("result = " + result);
     }
