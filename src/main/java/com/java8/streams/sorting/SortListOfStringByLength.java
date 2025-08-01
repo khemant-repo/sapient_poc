2fa05df21b08cb783813ab105d7671ab7769fb3f
Hemant Kumar <khemant.hubworks@gmail.com>
2025-08-01 15:58:49 +0530

    Added DSA problems on array and string: SortListOfStringByLength

---
+++ src/main/java/com/java8/streams/sorting/SortListOfStringByLength.java
@@ -0,0 +1,19 @@
+package com.java8.streams.sorting;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 17.	Given a list of strings, sort them according to increasing order of their length?
+ * a.	Input:  List<String> listOfStrings = Arrays.asList("Java", "Python", "C#", "HTML", "Kotlin", "C++", "COBOL", "C");
+ * b.	Output: C C# C++ Java HTML COBOL Python Kotlin
+ */
+public class SortListOfStringByLength {
+    public static void main(String[] args) {
+        List<String> listOfStrings = Arrays.asList("Java", "Python", "C#", "HTML", "Kotlin", "C++", "COBOL", "C");
+        var result  = listOfStrings.stream().sorted((s1,s2)-> s1.length() - s2.length()).collect(Collectors.toList());
+        System.out.println("result = " + result);
+        
+    }
+}
